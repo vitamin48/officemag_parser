3c71842faf6d6644138506d82d106474fd78eedc
Компьютер <vitamin48>
2023-03-12 20:31:22 +0300

    разбивка на классы

--- main_officemag_parser.py
+++ main_officemag_parser.py
@@ -6,10 +6,14 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 
-def write_txt_file_catalog_status(values):
-    with open("catalog_status_officemag.txt", "w") as output:
-        for row in values:
-            output.write(str(row) + '\n')
+class WriteFile:
+    def __init__(self, values):
+        self.values = values
+
+    def write_txt_file_catalog_status(self):
+        with open("catalog_status_officemag.txt", "w") as output:
+            for row in self.values:
+                output.write(str(row) + '\n')
 
 
 class ActualCatalog:
@@ -26,17 +30,29 @@ class ActualCatalog:
             registration = soup.find('div', class_='registrationHintDescription')
             if registration:
                 print('БАН')
-                break
+                return actual_catalog_list
             if sort:
                 url = self.catalog + f'{i}'
                 actual_catalog_list.append(url)
                 print(f'i = {i}, \n {url}')
-            #time.sleep(1)
+            # time.sleep(1)
         return actual_catalog_list
 
 
+class ParseDiscontProduct:
+    def __init__(self):
+        self.first_page = 'https://www.officemag.ru/promo/actions/?action_kind=manual&SORT=SORT&COUNT=60&PAGEN_1=2'
+
+
+def parse_actual_goods():
+    """Парсим товары перебором по его id. Если товар существует, добавляем ссылку на него в текстовый файл"""
+    actual_catalog_list = ActualCatalog().get_catalog_status(from_number=3000, to_number=3010)
+    WriteFile(values=actual_catalog_list).write_txt_file_catalog_status()
+
+
+def main():
+    parse_actual_goods()
+
+
 if __name__ == '__main__':
-    actual_catalog_list = ActualCatalog().get_catalog_status(from_number=1000, to_number=2000)
-    write_txt_file_catalog_status(values=actual_catalog_list)
-
-
+    main()
